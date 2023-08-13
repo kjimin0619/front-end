7ba5c350846e21fd5accba6304925f9d3a8f8352
kjimin0619 <lovejimin1578@gmail.com>
2023-08-13 18:48:48 +0900

    리액트 & 타입스크립트 & tailwind 개발환경 세팅

---
+++ Tailwind/react_tailwind_typescript/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex justify-center items-center">
+        <div className="container">test 1</div>
+        <div className="container">test 2</div>
+      </div>
+      <div className="text-3xl font-bold underline"> Hello wolrd ! </div>
+    </div>
+  );
+}
+
+export default App;
