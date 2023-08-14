5a2ae5b6b73fc65c3e18a95914d9fa623726896a
kjimin0619 <lovejimin1578@gmail.com>
2023-08-14 20:46:45 +0900

    플러그인, svg

--- Tailwind/react_tailwind_typescript/my-app/src/App.tsx
+++ Tailwind/react_tailwind_typescript/my-app/src/App.tsx
@@ -3,12 +3,95 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="flex justify-center items-center">
-        <div className="container">test 1</div>
-        <div className="container">test 2</div>
+    <div>
+      <div className="container">반응형 설계</div>
+      <div className="hide m-10">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dolore
+          explicabo facilis, quaerat deleniti eius beatae debitis aut quos ipsam
+          perferendis minima! Saepe, voluptas nemo dicta facilis quisquam
+          corrupti aut.
+        </p>
       </div>
-      <div className="text-3xl font-bold underline"> Hello wolrd ! </div>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6 stroke-emerald-600"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6 fill-blue-600"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5"
+        />
+      </svg>
+      <br></br>
+      {/* typography 플러그인 */}
+      <article className="prose lg:prose-md">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+      <br></br>
+
+      {/* form 플러그인 */}
+      <div className="m-3 space-x-4">
+        <input type="email" className="form-input rounded px-4 py-3" />
+        <select className="form-select rounded px-10 py-3">
+          <option>event1</option>
+          <option>event2</option>
+          <option>event3</option>
+          <option>event4</option>
+        </select>
+        <input
+          type="checkbox"
+          className="form-checkbox rounded text-pink-500"
+        />
+      </div>
+      <h1>hello world</h1>
+      <a href="#">
+        {/* <svg></svg> */}
+        <span className="sr-only">Settings</span>
+      </a>
     </div>
   );
 }
